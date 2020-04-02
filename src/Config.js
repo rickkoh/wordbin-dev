be40463bb146fc2691bb18872c7617dfaf44c47a
Rick Koh <realrickkoh@gmail.com>
2020-04-03 01:36:02 +0800

    created list of emitters.

---
+++ src/Config.js
@@ -0,0 +1,6 @@
+export const emitters = {
+    database_deleted: "database_deleted", // Called when database is deleted
+    database_changed: "database_changed", // Called when the database is udpated
+    word_changed: "word_changed", // Called when word is updated
+    meaning_changed: "meaning_changed", // Called when meaning is udpated
+}
--- src/Measurements.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Dimensions } from 'react-native';
-
-export const SCREEN_WIDTH = Dimensions.get('window').width;
